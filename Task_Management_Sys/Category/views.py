30999f3b55dc7276471d443c1ed685c5ce82f35d
ibnTofajjal <joynal.bintofajjal@gmail.com>
2024-06-09 20:05:15 +0600

    🐎 Render Forms To The Expected Page/Route

--- Task_Management_Sys/Category/views.py
+++ Task_Management_Sys/Category/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from . import forms
 
 # Create your views here.
 def add_category(request):
-    return HttpResponse("add add_category page")
+    if request.method == "POST":
+        category_form = forms.CategoryForm(request.POST)
+        if category_form.is_valid():
+            category_form.save()
+            return redirect("homepage")
+    else:
+        category_form = forms.CategoryForm()
+        return render(request, "add_category.html", {"forms": category_form})
--- Task_Management_Sys/Task/views.py
+++ Task_Management_Sys/Task/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from . import forms
 
 # Create your views here.
 def add_task(request):
-    return HttpResponse("add Task page")
+    if request.method == "POST":
+        taks_form = forms.TaskForm(request.POST)
+        if taks_form.is_valid():
+            taks_form.save()
+            return redirect("homepage")
+        
+    else:
+        taks_form = forms.TaskForm()
+        return render(request,"add_task.html", {"forms": taks_form})
