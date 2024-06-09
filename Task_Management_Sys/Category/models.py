68747189a222389890c950a6080f764d8a447b13
ibnTofajjal <joynal.bintofajjal@gmail.com>
2024-06-09 14:05:33 +0600

    🧑‍💻 Create Models and Forms With The Relationship Between Tasks and Categories

--- Task_Management_Sys/Category/models.py
+++ Task_Management_Sys/Category/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class CategoryModel(models.Model):
+    categoryName = models.CharField(max_length=100)
+    
+
+    def __str__(self) -> str:
+        return self.categoryName
--- Task_Management_Sys/Task/models.py
+++ Task_Management_Sys/Task/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from Category.models import CategoryModel
 # Create your models here.
+class TaskModel(models.Model):
+    taskTitle = models.CharField(max_length=255)
+    taskDescription = models.TextField()
+    is_completed = models.BooleanField(default=False)
+    taskAssigned =  models.DateField(None)
+    category = models.ManyToManyField(CategoryModel)
+
+    def __str__(self) -> str:
+        return self.taskTitle
