c24ee35e9ac5dcce208120baae20c24e29651652
ibnTofajjal <joynal.bintofajjal@gmail.com>
2024-06-10 00:11:44 +0600

    📝 Impletement Edit Task Functionality

--- Task_Management_Sys/Task/urls.py
+++ Task_Management_Sys/Task/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('add/', views.add_task, name="add_task"),
     path('delete/<int:id>', views.delete_task, name="delete_task"),
+    path('edit/<int:id>', views.edit_task, name="edit_task"),
 ]
--- Task_Management_Sys/Task/views.py
+++ Task_Management_Sys/Task/views.py
@@ -16,8 +16,22 @@ def add_task(request):
         return render(request,"add_task.html", {"forms": taks_form})
     
 
+def edit_task(request, id):
+    task = TaskModel.objects.get(pk=id) # Get the task object through the primary key
+    task_form = forms.TaskForm(instance=task) # create a instance of the task that get through the primary key
+
+    if request.method == "POST":
+        task_form = forms.TaskForm(request.POST, instance=task)
+        if task_form.is_valid():
+            task_form.save()
+            return redirect("homepage")
+        
+    return render(request,"add_task.html", {"forms": task_form})
+    
+
+
 
 def delete_task(request, id):
-    task = TaskModel.objects.get(pk=id)
+    task = TaskModel.objects.get(pk=id) # Get the Task object through the primary key
     task.delete()
     return redirect('homepage')
